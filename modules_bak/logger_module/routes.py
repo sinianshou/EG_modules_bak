e6076632468e0425b86eba034099a29ac8d496d4
Easer <>
2025-03-26 20:05:23 +0700

    添加swagger注释

--- modules_bak/logger_module/routes.py
+++ modules_bak/logger_module/routes.py
@@ -5,6 +5,22 @@ logger_bp = Blueprint('logger', __name__, url_prefix='/logger')
 
 @logger_bp.route('/status')
 def logger_status():
+    """获取日志模块状态
+    ---
+    tags:
+      - Logger
+    responses:
+      200:
+        description: 日志模块状态信息
+        schema:
+          properties:
+            status:
+              type: string
+              description: 模块运行状态
+            level:
+              type: string
+              description: 当前日志级别
+    """
     current_app.logger.debug('Debug log test')
     current_app.logger.info('Info log test')
     current_app.logger.warning('Warning log test')
